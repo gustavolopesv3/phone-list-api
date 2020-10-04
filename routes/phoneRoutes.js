12cc03defb0372f6a44481b8eb793c4d54138c4c
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2020-10-03 23:51:17 -0500

    criar novo contato

--- routes/phoneRoutes.js
+++ routes/phoneRoutes.js
@@ -9,4 +9,15 @@ router.get('/', async (req, res) => {
   res.send(data.phones);
 });
 
+//criar novo contato
+router.post('/', async (req, res) => {
+  let newContact = req.body;
+  const data = JSON.parse(await readFile('models/phonelist.json'));
+  newContact = { id: data.nextId, ...newContact };
+  data.nextId++;
+  data.phones.push(newContact);
+  await writeFile('models/phonelist.json', JSON.stringify(data, null, 2));
+  res.send(newContact);
+});
+
 export default router;

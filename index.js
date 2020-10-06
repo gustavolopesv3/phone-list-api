f7da696c081a0493a93f7175e62856b7f16aafab
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2020-10-06 17:26:54 -0500

    arquivo procfile para heroku

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import phoneRouter from './routes/phoneRoutes.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/phone', phoneRouter);
 app.listen(3000, () => {
